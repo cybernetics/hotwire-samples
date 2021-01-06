e1df748e30ef6271e96e165962946667c075609f
Josh Graham <jgraham@securecodewarrior.com>
2021-01-06 13:31:01 +1100

    Make ping() non-blocking. Add some simulated fun in ping times

--- src/main/kotlin/com/grahamis/hotwire/controllers/PingController.kt
+++ src/main/kotlin/com/grahamis/hotwire/controllers/PingController.kt
@@ -1,6 +1,7 @@
 package com.grahamis.hotwire.controllers
 
 import com.grahamis.CustomMediaType
+import kotlinx.coroutines.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
@@ -10,9 +11,14 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
+import reactor.core.publisher.Mono
 import java.io.IOException
 import java.net.InetSocketAddress
 import java.net.Socket
+import kotlin.math.absoluteValue
+import kotlin.random.Random
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
 
 
 @RequestMapping("/pinger")
@@ -24,59 +30,49 @@ class PingController {
     @Value("\${ping.port:8080}")
     private val port: Int = 8080
 
-    // Test will provide a mock bean, otherwise ping() will self-create a Socket
+    // Test will provide a `@MockBean`, otherwise create `Socket` when calling `ping(Socket)`
     @Autowired(required = false)
     private var socket: Socket? = null
 
-    @PostMapping(produces = ["${MediaType.TEXT_HTML_VALUE};turbo-stream", MediaType.TEXT_HTML_VALUE])
+    @ExperimentalTime
+    @PostMapping(produces = [CustomMediaType.TURBO_STREAM_VALUE, MediaType.TEXT_HTML_VALUE])
     @ResponseBody
-    fun pinger(): ResponseEntity<String> {
+    fun pinger(): Mono<ResponseEntity<String>> {
         val headers = HttpHeaders()
 
         headers.contentType = CustomMediaType.TURBO_STREAM
 
-        val ping = ping()
-        val pingTime = if (ping < 0) "timeout" else "$ping ms"
-
-        return ResponseEntity
-            .ok()
-            .contentType(CustomMediaType.TURBO_STREAM)
-            .body(
-                """
+        // socket will be a `@MockBean` during testing, null otherwise
+        return ping(socket ?: Socket()).map { duration ->
+            val pingTime = if (duration < 0) "timeout" else "$duration ms"
+            ResponseEntity
+                .ok()
+                .contentType(CustomMediaType.TURBO_STREAM)
+                .body(
+                    """
 <turbo-stream action="append" target="pings">
   <template>
     <li>$pingTime</li>
   </template>
 </turbo-stream>
                 """.trimIndent()
-            )
-    }
-
-    fun ping(): Long {
-        socket = socket ?: Socket()
-
-        val (reachable, time) = executeAndMeasureTimeMillis {
-            try {
-                socket!!.use {
-                    it.connect(InetSocketAddress(hostname, port), 1000)
-                    true
-                }
-            } catch (_: IOException) {
-                false
-            }
+                )
         }
-
-        /**
-         * The socket has been closed and it can only be used once
-         */
-        socket = null
-
-        return if (reachable) time else -1
     }
 
-    private inline fun <R> executeAndMeasureTimeMillis(fn: () -> R): Pair<R, Long> {
-        val start = System.currentTimeMillis()
-        val result = fn()
-        return result to (System.currentTimeMillis() - start)
+    @ExperimentalTime
+    fun ping(socket: Socket): Mono<Long> = Mono.create {
+        try {
+            it.success(measureTime {
+                socket.connect(InetSocketAddress(hostname, port), 10)
+                if (hostname == "127.0.0.1") {
+                    val sleep = Random.nextLong().absoluteValue % 10
+                    if (sleep % 5 == 0L) throw IOException() // force some timeouts now and then
+                    Thread.sleep(sleep)
+                }
+            }.toLongMilliseconds())
+        } catch (e: IOException) {
+            it.success(-1)
+        }
     }
 }
--- src/test/kotlin/com/grahamis/hotwire/controllers/PingControllerTest.kt
+++ src/test/kotlin/com/grahamis/hotwire/controllers/PingControllerTest.kt
@@ -3,20 +3,21 @@ package com.grahamis.hotwire.controllers
 import com.grahamis.CustomMediaType
 import com.grahamis.hasElement
 import com.grahamis.matches
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mockito.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.boot.test.mock.mockito.MockReset
 import org.springframework.http.MediaType
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.reactive.server.WebTestClient
 import java.io.IOException
-import java.net.InetAddress
-import java.net.InetSocketAddress
 import java.net.Socket
 import java.net.SocketAddress
+import kotlin.time.ExperimentalTime
 
 @ExtendWith(SpringExtension::class)
 @WebFluxTest(PingController::class)
@@ -24,10 +25,26 @@ class PingControllerTest {
     @Autowired
     private lateinit var webClient: WebTestClient
 
-    @MockBean
+    @MockBean(reset = MockReset.BEFORE)
     private lateinit var socket: Socket
 
     private val path = "/pinger"
+
+    @ExperimentalTime
+    @Test
+    fun shouldPing() {
+        mockSocketConnects()
+        val controller = PingController()
+        Assertions.assertThat(controller.ping(socket).block()).isGreaterThanOrEqualTo(0)
+    }
+
+    @ExperimentalTime
+    @Test
+    fun shouldTimeout() {
+        mockSocketTimeout()
+        val controller = PingController()
+        Assertions.assertThat(controller.ping(socket).block()).isEqualTo(-1)
+    }
 
     @Test
     fun shouldReturnTurboStreamContentType() {
@@ -49,13 +66,18 @@ class PingControllerTest {
 
     @Test
     fun shouldContainSomeTime() {
-        val response = response()
-        response.expectBody().xpath("//turbo-stream/template/li").matches("[0-9]+ ms")
+        mockSocketConnects()
+        response().expectBody().xpath("//turbo-stream/template/li").matches("[0-9]+ ms")
     }
 
     private fun mockSocketTimeout() {
         `when`(socket.connect(any(SocketAddress::class.java))).thenThrow(IOException())
         `when`(socket.connect(any(SocketAddress::class.java), anyInt())).thenThrow(IOException())
+    }
+
+    private fun mockSocketConnects() {
+        doNothing().`when`(socket).connect(any(SocketAddress::class.java))
+        doNothing().`when`(socket).connect(any(SocketAddress::class.java), anyInt())
     }
 
     private fun response() =
