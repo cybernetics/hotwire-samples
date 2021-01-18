c8b2413a72d9895abe9ba41482f6ce27e71c2283
Josh Graham <jgraham@securecodewarrior.com>
2021-01-18 11:09:51 +1100

    Use beta3 media type

--- src/main/kotlin/com/grahamis/CustomMediaType.kt
+++ src/main/kotlin/com/grahamis/CustomMediaType.kt
@@ -4,10 +4,7 @@ import org.springframework.http.MediaType
 
 class CustomMediaType(type: String) : MediaType(type) {
     companion object {
-        val TURBO_STREAM = MediaType(TEXT_HTML, mapOf("turbo-stream" to "*"))
-        const val TURBO_STREAM_VALUE = "$TEXT_HTML_VALUE; turbo-stream=*"
-//  TODO: remove above and uncomment below with Turbo 7 beta 3 https://github.com/hotwired/turbo/issues/24#issuecomment-754783881
-//        val TURBO_STREAM = MediaType("application", "vnd.turbo.stream.html")
-//        const val TURBO_STREAM_VALUE = "application/vnd.turbo.stream.html"
+        val TURBO_STREAM = MediaType("text", "vnd.turbo-stream.html")
+        const val TURBO_STREAM_VALUE = "text/vnd.turbo-stream.html"
     }
 }
